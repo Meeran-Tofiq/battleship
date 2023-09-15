b6e6313b2eef169c18e52db7c0bf3d5e42c0ec34
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-09-16 00:16:34 +0300

    Feat hits on ship

--- src/ship.js
+++ src/ship.js
@@ -1,6 +1,19 @@
 const shipFactory = (len) => {
     const length = len;
-    const hits = 0;
+    let hits = 0;
+
+    const hit = () => {
+        if (hits !== length) hits++;
+    };
+
+    const getHits = () => {
+        return hits;
+    };
+
+    return {
+        hit,
+        getHits,
+    };
 };
 
 export { shipFactory };
