92f2d995b75ba3e08bb26560c3f5c6baa21c0fa3
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-09-16 00:15:31 +0300

    Feat tests for hit method

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -1,1 +1,17 @@
+import { shipFactory } from '../src/ship';
 
+describe('hit', () => {
+    let ship;
+    beforeEach(() => (ship = shipFactory(1)));
+
+    it('adds a hit to hits variable', () => {
+        ship.hit();
+        expect(ship.getHits()).toBe(1);
+    });
+
+    it("doesn't add a hit when you reach the maximum", () => {
+        ship.hit();
+        ship.hit();
+        expect(ship.getHits()).toBe(1);
+    });
+});
