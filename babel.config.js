aa64d5c4da5c887ebaffaaa275546d8453953d81
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-09-15 23:57:03 +0300

    Feat plugin for dynamic imports in babel

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
     presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+    plugins: ['dynamic-import-node'],
+
+    env: {
+        test: {
+            plugins: ['dynamic-import-node'],
+        },
+    },
 };
