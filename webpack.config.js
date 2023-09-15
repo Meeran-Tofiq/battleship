815dd249d547692c402a0bdfdceb152b2742df41
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-09-15 23:21:47 +0300

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Weather App',
+        }),
+        new MiniCssExtractPlugin(),
+    ],
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    devServer: {
+        static: '/dist',
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+};
