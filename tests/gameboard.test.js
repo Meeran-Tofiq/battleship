78f8ac8ff7159ed9c4fa2f6fec39bef01a5ede60
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-09-16 12:28:14 +0300

    Feat tests for placeShip method

---
+++ tests/gameboard.test.js
@@ -0,0 +1,21 @@
+describe('placeShip', () => {
+    let board;
+    beforEach(() => (board = gameBoardFactory()));
+    it('places ship when spot is empty', () => {
+        expect(board.placeShip([1, 1], [3, 1])).toBe(true);
+    });
+
+    it('places ship in the correct spot', () => {
+        board.placeShip([1, 1], [1, 3]);
+        let arr = [];
+        for (let ind = 0; ind < 3; ind++) {
+            arr.push(board.isEmptyAt([1, ind]));
+        }
+        expect(arr).toBe([false, false, false]);
+    });
+
+    it("doesn't place ship when the spot is taken", () => {
+        board.placeShip([1, 1], [1, 3]);
+        expect(board.placeShip([1, 1], [1, 3])).toBe(false);
+    });
+});
