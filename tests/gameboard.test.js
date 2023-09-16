fd9f43bdefad259f5b445682b9b43a83340d4d40
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-09-16 14:12:05 +0300

    Feat tests for out of bound ship placement

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -3,21 +3,26 @@ import { gameBoardFactory } from '../src/gameboard';
 describe('placeShip', () => {
     let board;
     beforeEach(() => (board = gameBoardFactory()));
+
     it('places ship when spot is empty', () => {
         expect(board.placeShip([1, 1], [3, 1])).toBe(true);
-    });
-
-    it('places ship in the correct spot', () => {
-        board.placeShip([1, 1], [1, 3]);
-        let arr = [];
-        for (let ind = 0; ind < 3; ind++) {
-            arr.push(board.isEmptyAt([1, ind]));
-        }
-        expect(arr).toBe([false, false, false]);
     });
 
     it("doesn't place ship when the spot is taken", () => {
         board.placeShip([1, 1], [1, 3]);
         expect(board.placeShip([1, 1], [1, 3])).toBe(false);
+    });
+
+    it("doesn't place ship when it overlaps with another ship at 1 sqaure", () => {
+        board.placeShip([1, 1], [1, 3]);
+        expect(board.placeShip([1, 2], [3, 2])).toBe(false);
+    });
+
+    it("doesn't place ship when the x is outside the grid", () => {
+        expect(board.placeShip([11, 1], [1, 3])).toBe(false);
+    });
+
+    it("doesn't place ship when the y is outside the grid", () => {
+        expect(board.placeShip([1, 1], [12, 3])).toBe(false);
     });
 });
