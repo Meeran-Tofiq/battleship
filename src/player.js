744460fb776208a161b7a62c1130c9d7898b0cc4
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-09-23 21:39:28 +0300

    Feat to place random ships for a.i. board

--- src/player.js
+++ src/player.js
@@ -33,11 +33,44 @@ const playerFactory = (t, opp) => {
         }
     };
 
+    const placeRandomShips = () => {
+        [4, 3, 2, 2, 1].forEach((len) => {
+            let p1 = [
+                Math.floor(Math.random() * 10),
+                Math.floor(Math.random() * 10),
+            ];
+            let p2;
+            let done = false;
+            let vertical = Math.round(Math.random());
+
+            if (vertical) {
+                while (!done) {
+                    p1 = [
+                        Math.floor(Math.random() * 10),
+                        Math.floor(Math.random() * 10),
+                    ];
+                    p2 = [p1[0], p1[1] + len];
+                    done = board.placeShip(p1, p2);
+                }
+            } else {
+                while (!done) {
+                    p1 = [
+                        Math.floor(Math.random() * 10),
+                        Math.floor(Math.random() * 10),
+                    ];
+                    p2 = [p1[0] + len, p1[1]];
+                    done = board.placeShip(p1, p2);
+                }
+            }
+        });
+    };
+
     return {
         attack,
         setEnemy,
         getTurn,
         makeRandomAttack,
+        placeRandomShips,
         board,
     };
 };
