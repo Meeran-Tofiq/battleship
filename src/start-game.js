c68c13b6c56d4df821879cf34a98267aa742fce0
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-09-27 16:40:20 +0300

    Feat player input in the main game loop

--- src/start-game.js
+++ src/start-game.js
@@ -1,5 +1,6 @@
 import { placedShips } from './place-ships';
 import { playerFactory } from './player';
+import { getPlayerAttack, updateBoardWithAttack } from './ship-attacks';
 
 const setupGameButton = (btn) => {
     btn.addEventListener('click', () => {
@@ -7,15 +8,47 @@ const setupGameButton = (btn) => {
     });
 };
 
-const startGame = () => {
-    let player = playerFactory(true);
-    let opponent = playerFactory(false, player);
-    player.setEnemy(opponent);
+const startGame = async () => {
+    let player = playerFactory(false);
+    let opponent = playerFactory(true, player.board);
+    player.setEnemy(opponent.board);
 
     placedShips.forEach((ship) => player.board.placeShip(ship.p1, ship.p2));
     opponent.placeRandomShips();
 
-    console.log(opponent.board.print());
+    let playerBoard = document.querySelector('#player');
+    let opponetBoard = document.querySelector('#opponent');
+
+    let point;
+    while (true) {
+        if (player.getTurn()) {
+            try {
+                point = await getPlayerAttack(opponetBoard);
+            } catch (e) {
+                console.log(e);
+            }
+
+            let hitShip = opponent.board.hasShipAt(point);
+            let attack = player.attack(point);
+            updateBoardWithAttack(opponetBoard, point, hitShip);
+
+            if (attack) opponent.switchTurn();
+        } else if (opponent.getTurn()) {
+            point = opponent.makeRandomAttack();
+            let hitShip = player.board.hasShipAt(point);
+            updateBoardWithAttack(playerBoard, point, hitShip);
+
+            player.switchTurn();
+        }
+
+        if (opponent.board.haveLost()) {
+            console.log('YOU WON!');
+            break;
+        } else if (player.board.haveLost()) {
+            console.log('YOU LOST!');
+            break;
+        }
+    }
 };
 
 export { setupGameButton };
