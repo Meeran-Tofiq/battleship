ed563f31491f90182c3cffff804045722c0e2bcb
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-09-27 16:39:25 +0300

    Feat getting user input for where to hit as player

--- src/ship-attacks.js
+++ src/ship-attacks.js
@@ -1,7 +1,21 @@
-const updateBoardWithAttack = (board, p) => {
+const updateBoardWithAttack = (board, p, shipHit) => {
     let row = Array.from(board.querySelectorAll('.row'));
     let cell = row[p[0]].querySelector(`[x="${p[1]}"]`);
     cell.classList.add('hit');
 };
 
-export { updateBoardWithAttack };
+const getPlayerAttack = (board) => {
+    return new Promise((resolve, reject) => {
+        board.addEventListener('click', (event) => {
+            const clickedCell = event.target;
+            const x = parseInt(clickedCell.getAttribute('x'));
+            const y = parseInt(clickedCell.getAttribute('y'));
+
+            if (x > 9 || x < 0 || y > 9 || y < 0 || isNaN(x) || isNaN(y))
+                reject();
+            resolve([x, y]);
+        });
+    });
+};
+
+export { updateBoardWithAttack, getPlayerAttack };
