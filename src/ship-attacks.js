06e90ebfea01b81bbbb5d4ef9b7322943775b941
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-09-27 16:39:47 +0300

    Fix ship spots not being colored red

--- src/ship-attacks.js
+++ src/ship-attacks.js
@@ -1,7 +1,9 @@
 const updateBoardWithAttack = (board, p, shipHit) => {
     let row = Array.from(board.querySelectorAll('.row'));
-    let cell = row[p[0]].querySelector(`[x="${p[1]}"]`);
+    let cell = row[p[1]].querySelector(`[x="${p[0]}"]`);
     cell.classList.add('hit');
+    if (shipHit && !cell.classList.contains('taken'))
+        cell.classList.add('taken');
 };
 
 const getPlayerAttack = (board) => {
